4f4ec18e4e92e833f86ede1afe1aef1313584a7c
willwray <will@ripple.com>
2021-07-24 09:35:10 -0400

    Test: small msvc additions

--- tupl_test.cpp
+++ tupl_test.cpp
@@ -3,24 +3,45 @@
 
 using namespace ltl;
 
+template <typename T>
+constexpr std::add_const_t<T>& as_const(T& t) noexcept { return t; }
+
+#if defined (_MSC_VER)
+#define MSVC(...)__VA_ARGS__
+#else
+#define MSVC(...)
+#endif
+
 static_assert( tupl{1,2L,'3'} > tupl{1,2L,'2'} );
 
-int test_refs()
+bool test_refs()
 {
     int i;
     long j;
     char c;
-    tupl<int&,long&,char&> ijc{i,j,c};
-    ijc = {1,2L,'d'};
-    auto [x,y,z] = ijc;
-    return x;
+    tupl<int&,long&,char&> ijc{i,j,c}; // tie
+    ijc = {1,2L,'d'};  // assign-through (std tuple & tie don't have this)
+    auto& [x,y,z] = ijc;
+  //ijc = { x, y, z }; // FAIL: selects deleted default copy-assignment
+    as_const(ijc) = { x, y, z }; // Reference types are top-level const
+
+    assert(x == 1 && y == 2 && z == 'd');
+    assert(x == get<0>(ijc) && y == get<1>(ijc) && z == get<2>(ijc));
+    assert(&x == &get<0>(ijc) && &y == &get<1>(ijc) && &z == &get<2>(ijc));
+    assert((ijc == tupl<int&, long&, char&>{x, y, z}));
+    return true;
 };
 
 struct c16 { char data[16]; };
 struct BIG { char data[20]; };
 
-static_assert( std::is_aggregate_v<tupl<int&,long&,char&>> );
-//static_assert( std::is_trivial_v<tupl<int&,long&,char&>> );
+using tupl_scalar_refs = tupl<int&, long&, char&>;
+
+static_assert( std::is_aggregate<tupl_scalar_refs>() );
+static_assert(std::is_trivially_copyable<tupl_scalar_refs>());
+static_assert(!std::is_trivially_default_constructible<tupl_scalar_refs>());
+static_assert( MSVC(!) std::is_trivial_v<tupl_scalar_refs>);
+
 
 static_assert( std::same_as<tupl_ass_t<tupl<int&,long&,char&>>,
                                        tupl<int, long, char>> );
